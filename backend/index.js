3afb374817f9f8bedca6f86693f7008d974723a0
Anit Dias <anitdias3@gmail.com>
2024-03-30 10:24:36 +0530

    backend complete

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,18 @@ const express = require('express');
 const app = express();
 const {validateCreate,validateUpdate} = require('./middleware.js');
 const {todo} = require('./database.js');
+const cors = require('cors');
+
+app.use(express.json());
+app.use(cors());
 
 
 app.post('/createTodos',validateCreate,async function(req,res){
          let body = req.body;
          await todo.create({
           title: body.title,
-          description: body.description
+          description: body.description,
+          completed: false
          })
            
   
@@ -22,5 +27,17 @@ app.get('/listTodos',async function(req,res){
         })
      
 })
+
+app.put('/completed',validateUpdate,async function(req,res){
+  await todo.update({_id:request.body.id},
+    {completed:true})
+
+
+res.json({
+  msg:'todo marked as complete'
+})
+
+})
+
 
 app.listen(3000);
